bb5698abe206cf7624662541d08b89804fc4feb4
Shashank <shashank.kaphle@gmail.com>
2024-10-19 16:45:48 +0545

    enforce ssl for database connection

--- config/database.js
+++ config/database.js
@@ -3,14 +3,21 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  }
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASSWORD,
+	{
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				require: true, // Enforce SSL
+				rejectUnauthorized: false, // Skip certificate verification (optional; use if you're not using a self-signed cert)
+			},
+		},
+		logging: false,
+	}
 );
 
 module.exports = sequelize;
